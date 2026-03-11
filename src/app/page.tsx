72f58c2626d2f5b793f56ff3bd8493993387a0cf
bender <kudin.dima@gmail.com>
2026-03-11 17:22:31 +0000

    Update src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,7 +74,7 @@ export default function HomePage() {
           ]}
           enableKpiAnimation={true}
           buttons={[{ text: "Zadzwoń teraz", href: "tel:788595600" }]}
-          imageSrc="http://img.b2bpic.net/free-photo/man-drinking-hot-beverage-energy-crisis_23-2150061899.jpg"
+          imageSrc="http://img.b2bpic.net/free-photo/man-drinking-hot-beverage-energy-crisis_23-2150061899.jpg?_wi=1"
           imageAlt="Profesjonalna instalacja grzewcza"
           mediaAnimation="slide-up"
           imagePosition="right"
@@ -135,40 +135,40 @@ export default function HomePage() {
               title: "Grzewcze",              description:
                 "Montaż kotłów CO, grzejników, ogrzewania podłogowego, pomp ciepła i kolektorów. Budowa dużych kotłowni z kaskadą kotłów oraz modernizacja systemów grzewczych.",              phoneOne: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/view-exposed-plumbing-pipes_23-2150990679.jpg",                imageAlt: "Instalacja grzewcza"},
+                  "http://img.b2bpic.net/free-photo/view-exposed-plumbing-pipes_23-2150990679.jpg?_wi=1",                imageAlt: "Instalacja grzewcza"},
               phoneTwo: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/close-up-person-near-heater_23-2149284074.jpg",                imageAlt: "System grzewczy"},
+                  "http://img.b2bpic.net/free-photo/close-up-person-near-heater_23-2149284074.jpg?_wi=1",                imageAlt: "System grzewczy"},
             },
             {
               id: 2,
               title: "Gazowe",              description:
                 "Instalacja gazowa, montaż kotłów gazowych i przepływowych podgrzewaczy CWU. Usuwanie nieszczelności i przeglądy instalacji gazowych z certyfikacją.",              phoneOne: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/close-up-person-near-heater_23-2149284074.jpg",                imageAlt: "Instalacja gazowa"},
+                  "http://img.b2bpic.net/free-photo/close-up-person-near-heater_23-2149284074.jpg?_wi=2",                imageAlt: "Instalacja gazowa"},
               phoneTwo: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/female-mechanic-smiling-while-repairing-car_1170-1227.jpg",                imageAlt: "Usługa gazowa"},
+                  "http://img.b2bpic.net/free-photo/female-mechanic-smiling-while-repairing-car_1170-1227.jpg?_wi=1",                imageAlt: "Usługa gazowa"},
             },
             {
               id: 3,
               title: "Wodno-kanalizacyjne",              description:
                 "Nowe instalacje wodne i kanalizacyjne, naprawa istniejących instalacji. Montaż wodomierzy, zasobników i przepływowych podgrzewaczy.",              phoneOne: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/female-mechanic-smiling-while-repairing-car_1170-1227.jpg",                imageAlt: "Instalacja wodna"},
+                  "http://img.b2bpic.net/free-photo/female-mechanic-smiling-while-repairing-car_1170-1227.jpg?_wi=2",                imageAlt: "Instalacja wodna"},
               phoneTwo: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/man-worker-firld-by-solar-panels_1303-15560.jpg",                imageAlt: "Systemy odnawialne"},
+                  "http://img.b2bpic.net/free-photo/man-worker-firld-by-solar-panels_1303-15560.jpg?_wi=1",                imageAlt: "Systemy odnawialne"},
             },
             {
               id: 4,
               title: "Odnawialne źródła energii",              description:
                 "Kolektory słoneczne i zasobniki solarne. Rozwiązania ekologiczne zmniejszające koszty ogrzewania wody nawet o 60% i wspomagania centralnego ogrzewania o 30%.",              phoneOne: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/man-worker-firld-by-solar-panels_1303-15560.jpg",                imageAlt: "Systemy solarne"},
+                  "http://img.b2bpic.net/free-photo/man-worker-firld-by-solar-panels_1303-15560.jpg?_wi=2",                imageAlt: "Systemy solarne"},
               phoneTwo: {
                 imageSrc:
-                  "http://img.b2bpic.net/free-photo/man-worker-firld-by-solar-panels_1303-15556.jpg",                imageAlt: "Panele słoneczne"},
+                  "http://img.b2bpic.net/free-photo/man-worker-firld-by-solar-panels_1303-15556.jpg?_wi=1",                imageAlt: "Panele słoneczne"},
             },
           ]}
           showStepNumbers={true}
